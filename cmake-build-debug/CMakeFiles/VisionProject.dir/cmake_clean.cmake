1109c31cc084333e701cc299a246fb61feadd4de
markminch562 <markminch562@gmail.com>
2022-02-07 02:28:51 -0500

    small bracket error

---
+++ cmake-build-debug/CMakeFiles/VisionProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VisionProject.dir/main.cpp.o"
+  "VisionProject"
+  "VisionProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VisionProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/VisionProject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for VisionProject.
+# This may be replaced when dependencies are built.
